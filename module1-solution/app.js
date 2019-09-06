fb7175509829f2af590565af793ec851ad710fb7
Thirumalai <thirumalaitm205022@gmail.com>
2019-09-06 00:05:21 -0500

    updated the trim method in the app

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,38 +1,36 @@
 (function () {
-    'use strict';
-    
-    angular.module('MsgApp', [])
+  'use strict';
+
+  angular.module('MsgApp', [])
     .controller('LunchController', LunchController);
-    
-    LunchController.$inject = ['$scope'];
 
-    function LunchController($scope) {
-      
-        $scope.checkStatus = function () {
-         $scope.message = "";
-         $scope.inputClass = "inputGrey"
-            if($scope.lunch){
-                var splitLunch = $scope.lunch.split(',');
-                var splitLunchList = splitLunch.filter(function (element) {
-                    return (element != null && element!='') ;
-              });
+  LunchController.$inject = ['$scope'];
 
-                    if(splitLunchList.length > 3){
-                         $scope.message ="Too much!";
-                         $scope.messageClass="messageRed";
-                         $scope.inputClass="InputRed";
-                   }else if(splitLunchList.length <= 3){
-                         $scope.message ="Enjoy";
-                         $scope.messageClass="messageGreen";
-                         $scope.inputClass="InputGreen";
-                 }
-       
-            } else {
-               $scope.message ="Please Enter data first";
-            }
+  function LunchController($scope) {
 
-       
-        };
+    $scope.checkStatus = function () {
+      $scope.message = "";
+      $scope.inputClass = "inputGrey"
+      if ($scope.lunch) {
+        var splitLunch = $scope.lunch.split(',');
+        var splitLunchList = splitLunch.filter(function (element) {
+          element = element.trim();
+          return (element != null && element != '');
+        });
+        if (splitLunchList.length > 3) {
+          $scope.message = "Too much!";
+          $scope.messageClass = "messageRed";
+          $scope.inputClass = "InputRed";
+        } else if (splitLunchList.length <= 3) {
+          $scope.message = "Enjoy";
+          $scope.messageClass = "messageGreen";
+          $scope.inputClass = "InputGreen";
+        }
+      } else {
+        $scope.message = "Please Enter data first";
+      }
+
+
     };
-    })();
-    
+  };
+})();
